d284ea8bbbddf97d060276e9ef52aba45b2c9693
biagioo <mendolia2328@gmail.com>
2020-06-01 15:58:53 -0400

    created index and show methods for user and car, confirmed both are working as intended

--- backend/app/controllers/cars_controller.rb
+++ backend/app/controllers/cars_controller.rb
@@ -1,2 +1,12 @@
 class CarsController < ApplicationController
+
+    def index
+        cars = Car.all
+        render json: cars 
+    end
+
+    def show
+        car = Car.find_by(id: params[:id])
+        render json: car
+    end
 end
