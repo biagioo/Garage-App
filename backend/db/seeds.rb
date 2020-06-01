a9cc55b08f56d9e5ffa41d7dace9077cbb6c5548
biagioo <mendolia2328@gmail.com>
2020-06-01 15:50:50 -0400

    created seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+car_one = Car.new(year:2003, make: "Mitsubishi", model: "Lancer", trim: "Evolution GSR")
+car_two = Car.new(year:1998, make: "Toyota", model: "Supra", trim: "GR")
+car_three = Car.new(year:1999, make: "Nissan", model: "Skyline", trim: "GTR")
+u = User.create(username: "Future Biagio")
+
+car_one.user = u
+car_one.save
+
+car_two.user = u
+car_two.save 
+
+car_three.user = u
+car_three.save
