6eccc3c906da70345482353c664c0368bab4617b
biagioo <mendolia2328@gmail.com>
2020-06-01 18:00:04 -0400

    refactored html rendering for each car

--- frontend/index.js
+++ frontend/index.js
@@ -17,16 +17,17 @@ const renderUser = (user) => {
     // console.log(user)
     const div = document.createElement("div")
     const h3 = document.createElement("h4")
-    const ul = document.createElement("ul")  
+    // const ul = document.createElement("ul")  
 
     div.setAttribute("class", "garage")
 
     h3.innerText = user.username
+    h3.setAttribute('id', user.id)
 
-    ul.setAttribute("id", user.id)
+    // ul.setAttribute("id", user.id)
 
     div.appendChild(h3)
-    div.appendChild(ul)
+    // div.appendChild(ul)
 
     mainHTML.appendChild(div)
 
@@ -35,22 +36,24 @@ const renderUser = (user) => {
 }
 
 const renderCars = (car) => {
-    const ul = document.getElementById(`${car.user_id}`)
+    const h3 = document.getElementById(`${car.user_id}`)
+    const ul = document.createElement("ul")  
     console.log(car.year)
     const liMake = document.createElement('li')
     const liModel = document.createElement('li')
     const liYear = document.createElement('li')
     const liTrim = document.createElement('li')
     
-    liMake.innerText = `${car.make}`
-    liModel.innerText = `${car.model}`
-    liYear.innerText = `${car.year}`
-    liTrim.innerText = `${car.trim}`
+    liMake.innerText = `Manufacturer: ${car.make}`
+    liModel.innerText = `Model: ${car.model}`
+    liYear.innerText = `Year: ${car.year}`
+    liTrim.innerText = `Trim: ${car.trim}`
     
     ul.appendChild(liYear)
     ul.appendChild(liMake)
     ul.appendChild(liModel)
     ul.append(liTrim)
-
+    
+    h3.appendChild(ul)
     
 }
